10f3e2de5dc6f3802f03114f1e65f1e1f4c3e1c7
andres rodriguez <andresdrc28@gmail.com>
2026-08-10 18:04:11 -0400

    correicones para autodeploy

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
       disableErrorMessages: false, // Muestra mensajes de error detallados
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
